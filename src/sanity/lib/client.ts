8c7e3b9d162c265011d4200040a13f041353f447
Rajda-Hyder <rajdahyder@gmail.com>
2024-12-20 11:00:17 +0500

    Fix dependency issues for Vercel deployment

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,10 +1,10 @@
 import { createClient } from 'next-sanity'
 
-import { apiVersion, dataset, projectId } from '../env'
+//import { apiVersion, dataset, projectId } from '../env'
 
 export const client = createClient({
-  projectId,
-  dataset,
-  apiVersion,
+  projectId:'v8c2sipj',
+  dataset:'production',
+  apiVersion: '2021-10-21',
   useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
 })
